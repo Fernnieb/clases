541b5861fcaa7df6365f8f9c261f6bbfe9c62b56
Fernanda Bustos Reyes <Fernanda Bustos>
2024-01-31 15:43:46 -0300

    Comienzo

---
+++ Perfil.java
@@ -0,0 +1,31 @@
+package clases;
+
+public class Perfil {
+	
+	String esc;
+	
+	public void Nombre(String P){
+		
+		esc=P;
+	}
+				
+	public String Peso(){
+		
+		for(int i=2;i<esc.length();i++) {
+			
+			if(esc.substring(i,i+1).equals("X")==true) {
+			
+			String pp=(esc.substring(i+1, esc.length()));
+			
+			}
+		}
+	
+		return 	
+	}
+}
+
+
+	
+	
+		
+	
