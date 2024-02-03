69b9a7ea959daeaa87d55b0cd604aff8f19c784f
Fernanda Bustos <fernnieb@gmail.com>
2024-02-03 20:28:54 -0300

    supuestos

--- prueb1.java
+++ prueb1.java
@@ -8,5 +8,23 @@ public class prueb1 {
 		 Perfil miPerfil=new Perfil("HN20X50");
 	 System.out.println("La altura es:"+miPerfil.getAltura()*10);
 	 System.out.println("El peso es:"+miPerfil.getPeso());
+	
+	 double den=0.00785;
+	
+	 double Ancho, espesoralm, espesorala;
+	 
+	 // Primer supuesto
+	 
+	 Ancho=150;
+	 espesoralm=6;
+	 espesorala=6;
+	 
+	 double Area=(miPerfil.getAltura()*10-2*espesorala)*espesoralm+2*Ancho*espesorala;
+	 
+	 System.out.println("El area es: "+Area);
+	System.out.println("El peso del supuesto es: "+Area*den);
 	}
+	
+	
+	
 }
