cf40bec4dd788cd0e7c35cb60541b4b7af17e365
Fernanda Bustos <fernnieb@gmail.com>
2024-02-03 19:30:32 -0300

    comienzo

--- Perfil.java
+++ Perfil.java
@@ -2,25 +2,25 @@ package clases;
 
 public class Perfil {
 	
-	String esc;
+	static String esc;
+	String dato;
 	
-	public void Nombre(String P){
-		
-		esc=P;
-	}
-				
 	public String Peso(){
-		
+	
 		for(int i=2;i<esc.length();i++) {
 			
 			if(esc.substring(i,i+1).equals("X")==true) {
 			
-			String pp=(esc.substring(i+1, esc.length()));
-			
+				String pp=(esc.substring(i+1, esc.length()));
+				dato=pp;
 			}
+			else {
+				i=i+1;
+			}
+		
 		}
-	
-		return 	
+		
+		return dato;
 	}
 }
 
---
+++ prueb1.java
@@ -0,0 +1,22 @@
+package clases;
+
+public class prueb1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Perfil a1=new Perfil();
+		 a1="HN50X20";
+		
+	
+		 
+		 Nombre(p);
+	
+	
+		
+		
+	}
+
+
+
+}
