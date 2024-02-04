59ab03578b80a799a8eeb5da9d65aac208933d92
Fernanda Bustos <fernnieb@gmail.com>
2024-02-04 16:53:12 -0300

    dos digitos

--- prueb1.java
+++ prueb1.java
@@ -1,24 +1,31 @@
 package clases;
+import java.text.DecimalFormat;
+
+import javax.swing.*;
 
 public class prueb1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Perfil miPerfil=new Perfil("HN20X50");
+	 String p=JOptionPane.showInputDialog("Ingresa el Perfil");
+	 Perfil miPerfil=new Perfil(p);
 	 System.out.println("La altura es:"+miPerfil.getAltura()*10);
 	 System.out.println("El peso es:"+miPerfil.getPeso());
 	
 	 double den=0.00785;
 	
-	 double Ancho, espesoralm, espesorala;
+	 double espesoralm, espesorala;
+	 String An=JOptionPane.showInputDialog("Ingresa el ancho, mm:");
+	 double Ancho=Double.parseDouble(An);
 	 
 	 // Primer supuesto
 	 
-	 Ancho=150;
 	 espesoralm=6;
 	 double Area;
 	 double Peso;
+	 DecimalFormat formato=new DecimalFormat("0.00");
+	 
 	 for(int i=6;i<=22;i=i+2) {
 		 espesorala=i; 
 		 Area=(miPerfil.getAltura()*10-2*espesorala)*espesoralm+2*Ancho*espesorala;
@@ -27,11 +34,13 @@ public class prueb1 {
 		continue;
 	   }
 		 else{
-		System.out.println("El espesor del ala debe ser:"+i);
-		System.out.println("El peso del supuesto es: "+Peso);
+		espesorala=i;
+		System.out.println("El peso del supuesto es: "+formato.format(Peso));
+
 			 break;
 		 }
 	 }
+ 
 }
 	
 }
